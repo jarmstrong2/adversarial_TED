0a8aabcaa89f67397b8b13436c4dc5acf470054c
jarmstong2 <j2armstrong@gmail.com>
2016-07-16 22:19:15 -0400

    tested RNN_g

--- RNN_g.py
+++ RNN_g.py
@@ -1,3 +1,4 @@
+from RNN_t import *
 import tensorflow as tf
 import numpy as np
 
@@ -8,25 +9,24 @@ class RNN_g_model(object):
 		straight_size = config.straight_size
 		max_time_steps = config.max_time_steps
 		hidden_size = config.hidden_size_RNN_g
-		layer_size = config.layer_size_RNN_g
 		z_size = config.z_size
 		output_size_RNN_t = config.output_size_RNN_t
 
-		z = tf.placeholder(tf.float32, [batch_size, z_size])
+		self.z = tf.placeholder(tf.float32, [batch_size, z_size])
 
 		# linear trans for z -> straight vector
 		f_w = tf.get_variable("RNN_g_w", [z_size, straight_size])
 		f_b = tf.get_variable("RNN_g_b", [straight_size])
 
-		init_straight = tf.matmul(z, f_w) + f_b
+		init_straight = tf.matmul(self.z, f_w) + f_b
 
 		# linear trans for straight vector -> hidden_size of lstm
 		h_w = tf.get_variable("RNN_h_w", [straight_size + output_size_RNN_t, hidden_size])
 		h_b = tf.get_variable("RNN_h_b", [hidden_size])
 
 		# linear trans for hidden_size of lstm -> straight vector
-		j_w = tf.get_variable("RNN_j_w", [straight_size, hidden_size])
-		j_b = tf.get_variable("RNN_j_b", [hidden_size])
+		j_w = tf.get_variable("RNN_j_w", [hidden_size, straight_size])
+		j_b = tf.get_variable("RNN_j_b", [straight_size])
 
 		# maybe change bias?!
 		lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_size, forget_bias=0.0, state_is_tuple=True)
@@ -37,7 +37,10 @@ class RNN_g_model(object):
 		cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers_RNN_g, state_is_tuple=True)
 
 		outputs = []
-		cell_output = tf.matmul(tf.concat(1, [init_straight, RNN_t_output]), h_w) + h_b
+		concat_straight_RNN_t = tf.concat(1, [init_straight, RNN_t_output])
+		if is_training and config.keep_prob < 1:
+			concat_straight_RNN_t = tf.nn.dropout(concat_straight_RNN_t, config.keep_prob)
+		cell_output = tf.matmul(concat_straight_RNN_t, h_w) + h_b
 		state = cell.zero_state(batch_size, tf.float32)
 
 		# constructs the RNN_g
@@ -48,13 +51,45 @@ class RNN_g_model(object):
 		#	input : [(1) output time t, (2) output from RNN_t] -> [vector of size hidden_size]
 		with tf.variable_scope("RNN"):
 			for time_step in range(max_time_steps):
-				tf.get_variable_scope().reuse_variables()
+				if time_step > 0: tf.get_variable_scope().reuse_variables()
 				(cell_output, state) = cell(cell_output, state)
 				straight = tf.matmul(cell_output, j_w) + j_b
-				cell_output = tf.matmul(tf.concat(1, [straight, RNN_t_output]), h_w) + h_b
-				outputs.append(cell_output)
-
+				outputs.append(straight)
+				concat_straight_RNN_t = tf.concat(1, [straight, RNN_t_output])
+				if is_training and config.keep_prob < 1:
+					concat_straight_RNN_t = tf.nn.dropout(concat_straight_RNN_t, config.keep_prob)
+				cell_output = tf.matmul(concat_straight_RNN_t, h_w) + h_b
+				
 		self.outputs = outputs
+
+# testing if everything works
+if __name__ == "__main__" :
+	class configobj(object):
+		batch_size = 3
+		vocal_len = 30
+		hidden_size_RNN_t = 10
+		output_size_RNN_t = 40
+		num_layers_RNN_t = 3
+		keep_prob = 0.5
+		straight_size = 50
+		max_time_steps = 10
+		hidden_size_RNN_g = 200
+		num_layers_RNN_g = 3
+		z_size = 10
+
+	with tf.Graph().as_default(), tf.Session() as session:
+		initializer = tf.random_uniform_initializer(-0.4,0.4)
+		with tf.variable_scope("mod_t", reuse=None, initializer=initializer):
+			mod_t = RNN_t_model(True, configobj())
+		with tf.variable_scope("mod_g", reuse=None, initializer=initializer):
+			mod_g = RNN_g_model(True, configobj(), mod_t.outputs)
+		tf.initialize_all_variables().run()
+		u = np.eye(50,30)
+		u = np.expand_dims(u,0)
+		r = np.concatenate((u,u,u),axis=0)
+		z = np.random.randn(3, 10)
+		result = session.run((mod_g.outputs), {mod_t.cu:r, mod_g.z:z})
+		print(result)
 
 		
 
