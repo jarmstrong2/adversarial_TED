0dd3413dc108cb1216b4b76e09e9a283ab7c7a49
jarmstong2 <j2armstrong@gmail.com>
2016-07-26 08:09:56 -0400

    adjustment to update for gen

--- mnist_rnn.py
+++ mnist_rnn.py
@@ -219,7 +219,7 @@ if __name__ == "__main__" :
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
 		#lr = 0.005
-		lr = 0.0002
+		lr = 0.001
 		max_grad_norm = 10
 		iterations = 10**5
 		init_scale = 0.001
