0028c26393fc5d99a65e0c9746ccc1e5a500c355
jarmstong2 <j2armstrong@gmail.com>
2016-07-26 20:42:33 -0400

    change dropout prob

--- mnist_rnn.py
+++ mnist_rnn.py
@@ -205,7 +205,7 @@ if __name__ == "__main__" :
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 600
 		#lr = 0.005
-		lr = 0.0002
+		lr = 0.00005
 		max_grad_norm = 10
 		iterations = 10**6
 		init_scale = 0.001
