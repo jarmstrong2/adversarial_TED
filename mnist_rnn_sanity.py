e6b94feb4da6c27bf965ef5548285f6cb2614774
jarmstong2 <j2armstrong@gmail.com>
2016-07-29 19:10:05 -0400

    fix gen

--- mnist_rnn_sanity.py
+++ mnist_rnn_sanity.py
@@ -193,8 +193,8 @@ if __name__ == "__main__" :
 		hidden_size_RNN_g = 800
 		hidden_size_RNN_d = 800
 		#lr = 0.005
-		lr = 0.0002
-		max_grad_norm = 5
+		lr = 0.00002
+		max_grad_norm = 1
 		iterations = (10**6)
 		init_scale = 0.001
 
@@ -207,8 +207,8 @@ if __name__ == "__main__" :
 		hidden_size_RNN_g = 800
 		hidden_size_RNN_d = 800
 		#lr = 0.005
-		lr = 0.0002
-		max_grad_norm = 5
+		lr = 0.00002
+		max_grad_norm = 1
 		iterations = 10**5
 		init_scale = 0.01
 
