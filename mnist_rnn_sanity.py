37a6a108db04889875d689ea01217d89f466c248
jarmstong2 <j2armstrong@gmail.com>
2016-07-28 22:11:33 -0400

    fix gen

--- mnist_rnn_sanity.py
+++ mnist_rnn_sanity.py
@@ -93,8 +93,7 @@ class RNN_MNIST_model(object):
 			self.trainables_variables += lstm_variables
 
 			outputs_RNN_g = tf.transpose(output, perm=[1,0,2])
-			outputs_RNN_g = tf.sigmoid(outputs_RNN_g)
-			#outputs_RNN_g = tf.round(outputs_RNN_g)
+			outputs_RNN_g = tf.clip_by_value(outputs_RNN_g, 0, 256)
 
 			if model_type == "GEN":	
 				self.outputs = outputs_RNN_g
