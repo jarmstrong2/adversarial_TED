e4127baeea6a4a7674e9e1a7a12d813a04c521ab
jarmstong2 <j2armstrong@gmail.com>
2016-08-01 12:08:53 -0400

    new quad script

--- mnist_rnn.py
+++ mnist_rnn.py
@@ -263,7 +263,7 @@ if __name__ == "__main__" :
 		stepsingen_loss_d = 0
 
 		for i in range(configobj().iterations):
-			if ((i+1) % 1000 == 0):
+			if ((i+1) % 100 == 0):
 				print("------------")
 				print("Step: {}".format(i+1))
 				
