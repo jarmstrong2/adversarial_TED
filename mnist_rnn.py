11222a5db5f3045b0be2cd2f07503a607f90a5d1
jarmstong2 <j2armstrong@gmail.com>
2016-08-03 08:29:41 -0400

    larger g, smaller disc

--- mnist_rnn.py
+++ mnist_rnn.py
@@ -200,8 +200,8 @@ if __name__ == "__main__" :
 		batch_size = 2**7
 		keep_prob = 0.5
 		z_size = 100
-		lstm_layers_RNN_g = 8
-		lstm_layers_RNN_d = 4
+		lstm_layers_RNN_g = 6
+		lstm_layers_RNN_d = 2
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
 		lr = 0.0001
@@ -213,8 +213,8 @@ if __name__ == "__main__" :
 		batch_size = 2**7
 		keep_prob = 0.5
 		z_size = 100
-		lstm_layers_RNN_g = 8
-		lstm_layers_RNN_d = 4
+		lstm_layers_RNN_g = 6
+		lstm_layers_RNN_d = 2
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
 		lr = 0.0002
@@ -226,8 +226,8 @@ if __name__ == "__main__" :
 		batch_size = 2**6
 		keep_prob = 0.005
 		z_size = 100
-		lstm_layers_RNN_g = 8
-		lstm_layers_RNN_d = 4
+		lstm_layers_RNN_g = 6
+		lstm_layers_RNN_d = 2
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
 		lr = 0.0001
