c9ddf3d6b9749eb40b5714ea3a5b99fcdcf8e585
jarmstong2 <j2armstrong@gmail.com>
2016-08-28 22:35:26 -0400

    mnist

--- mnist_rnn_annealing_lr_with_separation.py
+++ mnist_rnn_annealing_lr_with_separation.py
@@ -236,8 +236,8 @@ if __name__ == "__main__" :
 		z_size = 100
 		lstm_layers_RNN_g = 6
 		lstm_layers_RNN_d = 4
-		hidden_size_RNN_g = 600
-		hidden_size_RNN_d = 400
+		hidden_size_RNN_g = 800
+		hidden_size_RNN_d = 800
 		lr = 0.0001
 		max_grad_norm = 10
 		iterations = (10**6)*3
@@ -249,8 +249,8 @@ if __name__ == "__main__" :
 		z_size = 100
 		lstm_layers_RNN_g = 6
 		lstm_layers_RNN_d = 4
-		hidden_size_RNN_g = 600
-		hidden_size_RNN_d = 400
+		hidden_size_RNN_g = 800
+		hidden_size_RNN_d = 800
 		lr = 0.0002
 		max_grad_norm = 10
 		iterations = (10**6)*3
@@ -262,8 +262,8 @@ if __name__ == "__main__" :
 		z_size = 100
 		lstm_layers_RNN_g = 6
 		lstm_layers_RNN_d = 4
-		hidden_size_RNN_g = 600
-		hidden_size_RNN_d = 400
+		hidden_size_RNN_g = 800
+		hidden_size_RNN_d = 800
 		lr = 0.0001
 		max_grad_norm = 10
 		iterations = (10**5)*6
@@ -336,7 +336,7 @@ if __name__ == "__main__" :
 				class_plt_d, = plt.plot(x_plot_class_d, y_plot_class_d, 'b-')
 				plt.legend([class_plt_g, class_plt_d], ["GEN", "DISC"])
 				plt.title('Classification')
-				plt.savefig('classification_sep_4_2.png')
+				plt.savefig('classification_sep_4_3.png')
 
 				x_plot_loss_g.append(i)
 				y_plot_loss_g.append(accumulator_loss_g/stepsingen_loss_g)
@@ -355,7 +355,7 @@ if __name__ == "__main__" :
 				loss_plt_d, = plt.plot(x_plot_loss_d, y_plot_loss_d, 'b-')
 				plt.legend([loss_plt_g, loss_plt_d], ["GEN", "DISC"])
 				plt.title('Loss')
-				plt.savefig('loss_sep_4_2.png')
+				plt.savefig('loss_sep_4_3.png')
 
 				y_plot_avg_g.append(getMovingAvg(trainable_g))
 				y_plot_avg_d.append(getMovingAvg(trainable_d))
@@ -364,7 +364,7 @@ if __name__ == "__main__" :
 				avg_plt_d, = plt.plot(x_plot_loss_d, y_plot_avg_d, 'b-')
 				plt.legend([avg_plt_g, avg_plt_d], ["GEN", "DISC"])
 				plt.title('Moving Average')
-				plt.savefig('avg_sep_4_2.png')
+				plt.savefig('avg_sep_4_3.png')
 
 
 
@@ -428,5 +428,5 @@ if __name__ == "__main__" :
 				stepsingen_loss_d += 1
 
 			if ((i+1) % 10000 == 0):
-				save_path = saver.save(session, "model_sep_4_2.ckpt")
+				save_path = saver.save(session, "model_sep_4_3.ckpt")
 				print("Model saved in file: %s" % save_path)
