7bd7d390a025d07b99a17cdc487a844e13c0258c
jarmstong2 <j2armstrong@gmail.com>
2016-09-05 10:58:59 -0400

    mnist

--- mnist_rnn_annealing_lr_with_separation.py
+++ mnist_rnn_annealing_lr_with_separation.py
@@ -251,7 +251,7 @@ if __name__ == "__main__" :
 		lstm_layers_RNN_d = 4
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
-		lr = 0.0002
+		lr = 0.0001
 		max_grad_norm = 10
 		iterations = (10**6)*3
 		init_scale = 0.01
