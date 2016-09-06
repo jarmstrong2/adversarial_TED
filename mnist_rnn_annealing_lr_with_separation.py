ec55d5a4dd89a3f0def106f3aa0db605ed07ca98
jarmstong2 <j2armstrong@gmail.com>
2016-09-06 08:05:22 -0400

    mnist

--- mnist_rnn_annealing_lr_with_separation.py
+++ mnist_rnn_annealing_lr_with_separation.py
@@ -238,7 +238,7 @@ if __name__ == "__main__" :
 		lstm_layers_RNN_d = 4
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
-		lr = 0.0001
+		lr = 0.0008
 		max_grad_norm = 10
 		iterations = (10**6)*3
 		init_scale = 0.01
@@ -251,7 +251,7 @@ if __name__ == "__main__" :
 		lstm_layers_RNN_d = 4
 		hidden_size_RNN_g = 600
 		hidden_size_RNN_d = 400
-		lr = 0.0002
+		lr = 0.0001
 		max_grad_norm = 10
 		iterations = (10**6)*3
 		init_scale = 0.01
@@ -338,7 +338,7 @@ if __name__ == "__main__" :
 				class_plt_d, = plt.plot(x_plot_class_d, y_plot_class_d, 'b-')
 				plt.legend([class_plt_g, class_plt_d], ["GEN", "DISC"])
 				plt.title('Classification')
-				plt.savefig('classification_sep_4_12.png')
+				plt.savefig('classification_sep_4_14.png')
 
 				x_plot_loss_g.append(i)
 				y_plot_loss_g.append(accumulator_loss_g/stepsingen_loss_g)
@@ -357,7 +357,7 @@ if __name__ == "__main__" :
 				loss_plt_d, = plt.plot(x_plot_loss_d, y_plot_loss_d, 'b-')
 				plt.legend([loss_plt_g, loss_plt_d], ["GEN", "DISC"])
 				plt.title('Loss')
-				plt.savefig('loss_sep_4_12.png')
+				plt.savefig('loss_sep_4_14.png')
 
 				y_plot_avg_g.append(getMovingAvg(trainable_g))
 				y_plot_avg_d.append(getMovingAvg(trainable_d))
@@ -366,7 +366,7 @@ if __name__ == "__main__" :
 				avg_plt_d, = plt.plot(x_plot_loss_d, y_plot_avg_d, 'b-')
 				plt.legend([avg_plt_g, avg_plt_d], ["GEN", "DISC"])
 				plt.title('Moving Average')
-				plt.savefig('avg_sep_4_12.png')
+				plt.savefig('avg_sep_4_14.png')
 
 
 
@@ -430,5 +430,5 @@ if __name__ == "__main__" :
 				stepsingen_loss_d += 1
 
 			if ((i+1) % 10000 == 0):
-				save_path = saver.save(session, "model_sep_4_12.ckpt")
+				save_path = saver.save(session, "model_sep_4_14.ckpt")
 				print("Model saved in file: %s" % save_path)
