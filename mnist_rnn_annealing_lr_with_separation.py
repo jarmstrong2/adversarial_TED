608f2b83d283e23c6b3dd41d8703ef69d0704e4d
jarmstong2 <j2armstrong@gmail.com>
2016-09-11 09:06:17 -0400

    mnist

--- mnist_rnn_annealing_lr_with_separation.py
+++ mnist_rnn_annealing_lr_with_separation.py
@@ -236,12 +236,12 @@ if __name__ == "__main__" :
 		z_size = 100
 		lstm_layers_RNN_g = 6
 		lstm_layers_RNN_d = 2
-		hidden_size_RNN_g = 800
-		hidden_size_RNN_d = 600
+		hidden_size_RNN_g = 600
+		hidden_size_RNN_d = 400
 		lr = 0.0001
 		max_grad_norm = 10
 		iterations = (10**6)*3
-		init_scale = 0.01
+		init_scale = 0.001
 
 	class configobj_f(object):
 		batch_size = 2**7
@@ -249,12 +249,12 @@ if __name__ == "__main__" :
 		z_size = 100
 		lstm_layers_RNN_g = 6
 		lstm_layers_RNN_d = 2
-		hidden_size_RNN_g = 800
-		hidden_size_RNN_d = 600
+		hidden_size_RNN_g = 600
+		hidden_size_RNN_d = 400
 		lr = 0.0002
 		max_grad_norm = 10
 		iterations = (10**6)*3
-		init_scale = 0.01
+		init_scale = 0.001
 
 	class configobj_g(object):
 		batch_size = 2**6
@@ -262,12 +262,12 @@ if __name__ == "__main__" :
 		z_size = 100
 		lstm_layers_RNN_g = 6
 		lstm_layers_RNN_d = 2
-		hidden_size_RNN_g = 800
-		hidden_size_RNN_d = 600
+		hidden_size_RNN_g = 600
+		hidden_size_RNN_d = 400
 		lr = 0.0001
 		max_grad_norm = 10
 		iterations = (10**5)*6
-		init_scale = 0.01
+		init_scale = 0.001
 
 
 	with tf.Graph().as_default(), tf.Session() as session:
@@ -343,7 +343,7 @@ if __name__ == "__main__" :
 				class_plt_d, = plt.plot(x_plot_class_d, y_plot_class_d, 'b-')
 				plt.legend([class_plt_g, class_plt_d], ["GEN", "DISC"])
 				plt.title('Classification')
-				plt.savefig('classification_sep_7_2.png')
+				plt.savefig('classification_sep_7_1.png')
 
 				x_plot_loss_g.append(i)
 				y_plot_loss_g.append(accumulator_loss_g/stepsingen_loss_g)
@@ -362,7 +362,7 @@ if __name__ == "__main__" :
 				loss_plt_d, = plt.plot(x_plot_loss_d, y_plot_loss_d, 'b-')
 				plt.legend([loss_plt_g, loss_plt_d], ["GEN", "DISC"])
 				plt.title('Loss')
-				plt.savefig('loss_sep_7_2.png')
+				plt.savefig('loss_sep_7_1.png')
 
 				y_plot_avg_g.append(getMovingAvg(trainable_g))
 				y_plot_avg_d.append(getMovingAvg(trainable_d))
@@ -371,7 +371,7 @@ if __name__ == "__main__" :
 				avg_plt_d, = plt.plot(x_plot_loss_d, y_plot_avg_d, 'b-')
 				plt.legend([avg_plt_g, avg_plt_d], ["GEN", "DISC"])
 				plt.title('Moving Average')
-				plt.savefig('avg_sep_7_2.png')
+				plt.savefig('avg_sep_7_1.png')
 
 
 
@@ -436,5 +436,5 @@ if __name__ == "__main__" :
 				stepsingen_loss_d += 1
 
 			if ((i+1) % 10000 == 0):
-				save_path = saver.save(session, "model_sep_7_2.ckpt")
+				save_path = saver.save(session, "model_sep_7_1.ckpt")
 				print("Model saved in file: %s" % save_path)
